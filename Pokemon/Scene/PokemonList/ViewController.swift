ca7e7dc81f068b452be71426b8d61ab1cd19a6ca
baris <bdilekci200@gmail.com>
2023-12-13 22:32:21 +0300

    feat: pokemon data was shown in the log section

--- Pokemon/Scene/PokemonList/ViewController.swift
+++ Pokemon/Scene/PokemonList/ViewController.swift
@@ -8,13 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private let viewModel = PokemonListViewModel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         view.backgroundColor = .red
+        viewModel.delegate = self
+        viewModel.fetchPokemonData()
     }
 
 
 }
 
+extension ViewController: IPokemonListViewModel {
+    func didErrorList(error: String) {
+        print("error")
+    }
+    
+    func didSuccess() {
+        DispatchQueue.main.async {
+            self.viewModel.pokemons?.results.forEach({ result in
+                print(result)
+            })
+        }
+    }
+    
+    
+}
